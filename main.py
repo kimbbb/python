0719dfb0402dd82856f7a766494bd9496ad00cd8
김민지 <wehouse201@naver.com>
2022-07-29 20:55:57 +0900

    for

--- main.py
+++ main.py
@@ -31,3 +31,119 @@
 # a=input()
 # a=int(a)
 
+# 합 구하기
+# a=int(input())
+# b=int(input())
+# sum=a+b
+# print(sum)
+
+# 일의 자리 십의 자리
+# import math
+#
+# a=int(input())
+# print("십의 자리 : ", math.trunc((a%100)/10))
+# print('십의 자리 : ', a%10)
+
+
+# 자동차 연비 계산
+# start=int(input())
+# end=int(input())
+# write=int(input())
+#
+# cash=(end-start)/write
+#
+# print('연비는', round(cash),'km / 1입니다.')
+
+# 1-dog, 2-cat, 3-chick
+# a=int(input())
+#
+# if a==1:
+#     print("dog")
+# elif a==2:
+#     print("cat")
+# elif a==3:
+#     print("chick")
+
+# m=0
+# for i in range(5):
+#     a=int(input())
+#
+# if a>m:
+#     m=a
+#
+# print(m)
+
+# a = int(input())
+#
+# if a%2==0:
+#     print("짝수")
+# elif a%2!=0:
+#     print("홀수")
+# elif a==0:
+#     print(a)
+
+# m=0
+# for i in range(5):
+#     a=int(input())
+#
+#     if a>m:
+#         m=a
+#
+#     if a%2!=0:
+#         m=a
+#
+# print(m)
+
+# N=int(input())
+# for i in range(1,N+1):
+#     print(i)
+
+# a=int(input())
+# for i in range(1,10):
+#     print(a,"*",i,"=",a*i)
+
+# sum=0
+# N=int(input())
+# for i in range(N+1):
+#     sum+=i
+#
+# print(sum)
+
+# odd=0
+# even=0
+# for i in range(5):
+#     a=int(input())
+#
+#     if a%2==0:
+#         even+=1
+#     else:
+#         odd+=1
+#
+# print("홀수 :",odd)
+# print("짝수 :",even)
+
+# sum=0
+# for i in range(6):
+#      a=int(input())
+#
+#      sum+=a
+#
+# print(sum/6)
+
+# sum=0
+# N=int(input())
+# for i in range(N):
+#     a=int(input())
+#     sum+=a
+# print(sum)
+
+# a=int(input())
+# for i in range(1,a):
+#     if a%i==0 :
+#         print(i, end=" ")
+
+# a,b=map(int,input().split())
+#
+# for i in range(1,a*b):
+#     if a%i==0 and b%i==0:
+#         print(i, end=" ")
