9d877f8d3760238ee3002aa90f31fa8470deeaaf
김민지 <wehouse201@naver.com>
2022-07-29 14:50:09 +0900

    input

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
-print("""줄바꿈된당
-ㅋㅋ
-메롱
-바보야!
-""")
+# print("\"안녕? 나는 철수야.\"라고 말했다.")
+
+# print("재고리스트")
+# print("품번\t품목명\t수량\t단가")
+# print("N01\t연필심\t3EA\t100$")
+# print("N02\t필통ㅋ\t5EA\t400$")
+
+# result = 1
+# result *= 2
+# result *= 2
+# result /= 4
+# print(result)
+
+# result =1+2*3-(4//5)
+# print(result)
+
+# result=7
+# print(result%2)
+# result//=2
+# print(result%2)
+# result//=2
+# print(result%2)
+
+# result=1024
+# result%=2
+# result%=2
+# result%=2
+# print(result)
+
+# float(input())
+# a=input()
+# a=int(a)
+
