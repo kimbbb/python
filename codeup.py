aac59040eb63bac0c24ac8d9a1419bd837220f2d
김민지 <wehouse201@naver.com>
2022-08-22 19:11:32 +0900

    codeup

---
+++ codeup.py
@@ -0,0 +1,96 @@
+#https://codeup.kr/problemsetsol.php?psid=33
+
+# #1
+# print("Hello")
+#
+# #2
+# print("Hello","World")
+#
+# #3
+# print("Hello")
+# print("World")
+#
+# #4
+# print("'Hello'")
+#
+# #5
+# print('"Hello World"')
+#
+# #6
+# print('\"!@#$%^&*()\'')
+#
+# #7
+# print("\"C:\\Download\\'hello\'.py\"")
+#
+# #8
+# print("print(\"Hello\\nWorld\")")
+
+#입력
+# #9
+# a=input()
+# print(a)
+#
+# #10
+# a=input()
+# a=int(a)
+# print(a)
+#
+# #11
+# a=input()
+# a=float(a)
+# print(a)
+#
+# #12
+# a=int(input())
+# b=int(input())
+# print(a)
+# print(b)
+#
+# #13
+# a=input()
+# b=input()
+# print(b)
+# print(a)
+#
+# #14
+# f=float(input())
+# print(f)
+# print(f)
+# print(f)
+#
+# #15
+# a,b=map(int, input().split())
+# print(a)
+# print(b)
+#
+# #16
+# a,b=(input().split())
+# print(b,a)
+#
+# #17
+# a=input()
+# print(a,a,a)
+#
+# #18
+# a, b = input().split(':')
+# print(a, b, sep=':')
+#
+# #19
+# y, m, d = input().split('.')
+# print(d,m,y,sep='-')
+#
+# #20
+# a, b = input().split('-')
+# print(a+b)
+#
+# #21
+# a=input()
+# print(a[0])
+# print(a[1])
+# print(a[2])
+# print(a[3])
+# print(a[4])
+
+#22
+a=input().split()
+print(a[0:])
